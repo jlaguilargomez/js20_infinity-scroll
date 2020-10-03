89a64bbe375c5dcba3a7707c3d6c8b4bd1109715
jlaguilargomez <jlaguigom@gmail.com>
2020-10-03 09:26:08 +0200

    Create img and anchor tag element and put into image-container element

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ const apiKey = 'ubu9lCAZ2x4TJmExp3iAPqMVFViYoNjKxMaAWMkJos4';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Create Elements for Links & photos add to the DOM
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a');
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', '_blank');
 
+    // Create <img> for photo
+    const img = document.createElement('img');
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description);
+    img.setAttribute('title', photo.alt_description);
+
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item)
+    
+  });
+}
 
 // Get photos from Unsplash API
 async function getPhotos(){
