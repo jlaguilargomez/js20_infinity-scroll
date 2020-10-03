0649b58cd78c4508d55c487f45570d0fec1d0ca3
jlaguilargomez <jlaguigom@gmail.com>
2020-10-03 09:44:15 +0200

    DRY on displayPhotos function!

--- script.js
+++ script.js
@@ -8,24 +8,30 @@ const count = 100;
 const apiKey = 'ubu9lCAZ2x4TJmExp3iAPqMVFViYoNjKxMaAWMkJos4';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+
+
 // Create Elements for Links & photos add to the DOM
+
+function setAttributes(elem, attribute, prop) {
+  return elem.setAttribute(attribute, prop)
+}
+
 function displayPhotos() {
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
-
+    setAttributes(item, 'href',photo.links.html)
+    setAttributes(item, 'target', '_blank');
+    
     // Create <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
-
+    setAttributes(img, 'src', photo.urls.regular);
+    setAttributes(img, 'alt', photo.alt_description);
+    setAttributes(img, 'title', photo.alt_description);
+    
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item)
-    
   });
 }
 
